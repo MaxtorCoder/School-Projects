a80b8e8f9dfc0dbbdbc7a5c3647b6f6dddb2ab01
MaxtorCoder <warsongkiller.s8@gmail.com>
2020-03-31 10:57:09 +0200

    Rewrite Roman numeral handler

---
+++ RomanNumerals/RomanNumerals.GUI/Roman.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RomanNumerals.GUI
+{
+    public class Roman
+    {
+        public static Dictionary<int, string> RomanNumerals = new Dictionary<int, string>
+        {
+            { 1000, "M" },
+            { 500,  "D" },
+            { 100,  "C" },
+            { 50,   "L" },
+            { 10,   "X" },
+            { 5,    "V" },
+            { 1,    "I" },
+        };
+        public static Dictionary<int, string> RomanNumeralRules = new Dictionary<int, string>
+        {
+            { 900,  "CM" },
+            { 400,  "CD" },
+            { 90,   "XC" },
+            { 40,   "XL" },
+            { 9,    "IX" },
+            { 4,    "IV" },
+        };
+
+        public string NumberToRoman(int input)
+        {
+            var returnString = string.Empty;
+            while (input > 0)
+            {
+                foreach (var romanNumeral in RomanNumerals)
+                {
+                    if (RomanNumeralRules.ContainsKey(input))
+                    {
+                        returnString += RomanNumeralRules[input];
+                        input -= input;
+                        break;
+                    }
+                    else if (input >= romanNumeral.Key)
+                    {
+                        input -= romanNumeral.Key;
+                        returnString += romanNumeral.Value;
+                        break;
+                    }
+                }
+            }
+
+            return returnString;
+        }
+
+        public int RomanToNumber(string input)
+        {
+            var result = 0;
+
+            foreach (var character in input)
+            {
+                var index = input.IndexOf(character);
+
+                if ((index + 1) >= input.Length)
+                    break;
+
+                var romanString = $"{character}{input[index + 1]}";
+                var romanNumeral = 0;
+
+                if (RomanNumeralRules.ContainsValue(romanString))
+                    romanNumeral = RomanNumeralRules.FirstOrDefault(x => x.Value == romanString).Key;
+                else
+                    romanNumeral = RomanNumerals.FirstOrDefault(x => x.Value == character.ToString()).Key;
+
+                result += romanNumeral;
+            }
+
+            return result;
+        }
+    }
+}
--- RomanNumerals/RomanNumerals/Program.cs
+++ RomanNumerals/RomanNumerals/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data;
 
 namespace RomanNumerals
 {
@@ -41,7 +39,7 @@ namespace RomanNumerals
             Console.Clear();
             Console.Write("Please input a roman numeral to convert to an integer: ");
 
-            var input = Console.ReadLine();
+            var input = Console.ReadLine().ToUpper();
             var roman = new Roman();
 
             var output = roman.RomanToNumber(input);
@@ -52,54 +50,6 @@ namespace RomanNumerals
 
             Console.WriteLine($"Output: {output}");
             Console.ReadKey();
-        }
-    }
-
-    public class Roman
-    {
-        public string NumberToRoman(int input)
-        {
-            if (input >= 1000) return "M"  + NumberToRoman(input - 1000);
-            if (input >= 900)  return "CM" + NumberToRoman(input - 900);
-            if (input >= 500)  return "D"  + NumberToRoman(input - 500);
-            if (input >= 400)  return "CD" + NumberToRoman(input - 400);
-            if (input >= 100)  return "C"  + NumberToRoman(input - 100);
-            if (input >= 90)   return "XC" + NumberToRoman(input - 90);
-            if (input >= 50)   return "L"  + NumberToRoman(input - 50);
-            if (input >= 40)   return "XL" + NumberToRoman(input - 40);
-            if (input >= 10)   return "X"  + NumberToRoman(input - 10);
-            if (input >= 9)    return "IX" + NumberToRoman(input - 9);
-            if (input >= 5)    return "V"  + NumberToRoman(input - 5);
-            if (input >= 4)    return "IV" + NumberToRoman(input - 4);
-            if (input >= 1)    return "I"  + NumberToRoman(input - 1);
-            if (input == 0)    return string.Empty;
-            throw new ArgumentException("Please input a number in between 1 - 4999");
-        }
-
-        public int RomanToNumber(string input)
-        {
-            var result = 0;
-
-            // Convert any hidden cases to the correct roman numerals.
-            input = input.Replace("CM", "DCCCC");
-            input = input.Replace("CD", "CCCC");
-            input = input.Replace("XC", "LXXXX");
-            input = input.Replace("XL", "XXXX");
-            input = input.Replace("IX", "VIIII");
-            input = input.Replace("IV", "IIII");
-
-            foreach (var character in input)
-            {
-                if (character == 'I') result += 1;
-                if (character == 'V') result += 5;
-                if (character == 'X') result += 10;
-                if (character == 'L') result += 50;
-                if (character == 'C') result += 100;
-                if (character == 'D') result += 500;
-                if (character == 'M') result += 1000;
-            }
-
-            return result;
         }
     }
 }
